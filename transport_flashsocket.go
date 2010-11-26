21d21e0fa832e63f5428c9ab51d24964b509b126
Jukka-Pekka Kekkonen <karatepekka@gmail.com>
2010-11-26 23:39:52 +0200

    Move flashsocketpolicyserver here

--- transport_flashsocket.go
+++ transport_flashsocket.go
@@ -3,6 +3,11 @@ package socketio
 import (
 	"http"
 	"os"
+	"bytes"
+	"strings"
+	"fmt"
+	"net"
+	"io"
 )
 
 // The flashsocket transport.
@@ -59,4 +64,86 @@ func (s *flashsocketSocket) Write(p []byte) (int, os.Error) {
 
 func (s *flashsocketSocket) Close() os.Error {
 	return s.s.Close()
+}
+
+func generatePolicyFile(sio *SocketIO) []byte {
+	buf := new(bytes.Buffer)
+	buf.WriteString(`<?xml version="1.0"?>
+<!DOCTYPE cross-domain-policy SYSTEM "http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">
+<cross-domain-policy>
+	<site-control permitted-cross-domain-policies="master-only" />
+`)
+
+	if sio.config.Origins != nil {
+		for _, origin := range sio.config.Origins {
+			parts := strings.Split(origin, ":", 2)
+			if len(parts) < 1 {
+				continue
+			}
+			host, port := "*", "*"
+			if parts[0] != "" {
+				host = parts[0]
+			}
+			if len(parts) == 2 && parts[1] != "" {
+				port = parts[1]
+			}
+
+			fmt.Fprintf(buf, "\t<allow-access-from domain=\"%s\" to-ports=\"%s\" />\n", host, port)
+		}
+	}
+
+	buf.WriteString("</cross-domain-policy>\n")
+	return buf.Bytes()
+}
+
+func (sio *SocketIO) ListenAndServeFlashPolicy(laddr string) os.Error {
+	var listener net.Listener
+
+	listener, err := net.Listen("tcp", laddr)
+	if err != nil {
+		return err
+	}
+
+	policy := generatePolicyFile(sio)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			sio.Log("ServeFlashsocketPolicy:", err)
+			continue
+		}
+
+		go func() {
+			defer conn.Close()
+
+			buf := make([]byte, 20)
+			if _, err := io.ReadFull(conn, buf); err != nil {
+				sio.Log("ServeFlashsocketPolicy:", err)
+				return
+			}
+			if !bytes.Equal([]byte("<policy-file-request"), buf) {
+				sio.Logf("ServeFlashsocketPolicy: expected \"<policy-file-request\" but got %q", buf)
+				return
+			}
+
+			var nw int
+			for nw < len(policy) {
+				n, err := conn.Write(policy[nw:])
+				if err != nil && err != os.EAGAIN {
+					sio.Log("ServeFlashsocketPolicy:", err)
+					return
+				}
+				if n > 0 {
+					nw += n
+					continue
+				} else {
+					sio.Log("ServeFlashsocketPolicy: wrote 0 bytes")
+					return
+				}
+			}
+			sio.Log("ServeFlashsocketPolicy: served", conn.RemoteAddr())
+		}()
+	}
+
+	return nil
 }
